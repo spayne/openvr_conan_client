6fa0127b21f5840d2cacfdd0e42b9d250df51c7f
Sean Payne <sean.d.payne@gmail.com>
2020-02-24 16:04:43 -0800

    add powershell script to test debug build

---
+++ build_debug.ps1
@@ -0,0 +1,35 @@
+#
+# Purpose: 
+# Mechanize the process of building a debug project using conan,cmake for Visual Studio
+#
+#
+
+Function Log($message) {
+Write-Host
+Write-Host
+Write-Host "================================================================================================"
+Write-Host "=" $message 
+Write-Host "================================================================================================"
+}
+
+$start_location = Get-Location
+$build_folder = 'debug'
+
+Log "Creating $build_folder folder"
+if (Test-Path $build_folder) 
+{
+    Remove-Item –recurse -force $build_folder
+}
+New-Item -ItemType "directory" $build_folder
+cd $build_folder
+
+Log "Setting up conan in $build_folder folder"
+conan install .. -s build_type=Debug
+Log "Setting up cmake in $build_folder folder"
+cmake .. -G "Visual Studio 16" 
+Log "Test build in $build_folder folder"
+cmake --build . --config Debug
+
+Log "Build results:"
+Set-Location $start_location
+dir $build_folder/bin
